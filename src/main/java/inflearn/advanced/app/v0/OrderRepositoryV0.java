611b3cc4ca791c8701113f85cae9c14b1751fec1
Jaesuk <jaesuk95@outlook.com>
2022-07-17 19:51:07 +0900

    V0 - simple structure

---
+++ src/main/java/inflearn/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,26 @@
+package inflearn.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    // 상품 저장하는데 1초 걸림
+    public void save(String itemId){
+        // 저장 로직
+        if (itemId.equals("ex")){
+            throw new IllegalStateException("exception");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}

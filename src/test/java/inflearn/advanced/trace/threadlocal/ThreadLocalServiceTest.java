b7006bb54f40883a83a6a62caecd396508e22b8e
Jaesuk <jaesuk95@outlook.com>
2022-07-19 23:56:53 +0900

    ThreadLocalService (동시성) test (테스트 완성)

---
+++ src/test/java/inflearn/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package inflearn.advanced.trace.threadlocal;
+
+import inflearn.advanced.trace.threadlocal.code.FieldService;
+import inflearn.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);
+        sleep(100);
+        threadB.start();
+
+        sleep(3000);    // main thread 종료 대기
+        log.info("mainExit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/inflearn/advanced/trace/threadlocal/code/ThreadLocalService.java
+++ src/test/java/inflearn/advanced/trace/threadlocal/code/ThreadLocalService.java
@@ -12,10 +12,10 @@ public class ThreadLocalService {
     private ThreadLocal<String> nameStore = new ThreadLocal<>();
 
     public String logic(String name){
-        log.info("저장 name={} -> nameStore", name, nameStore.get());
+        log.info("저장 name={} -> nameStore={}", name, nameStore.get());
         nameStore.set(name);
         sleep(1000);
-        log.info("조회 nameStore={}",nameStore);
+        log.info("조회 nameStore={}",nameStore.get());
         return nameStore.get();
     }
 
